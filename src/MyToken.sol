3908ebd52dc0c611b183cfe08251525c2e8c812f
Sky XYZ <jay@wild.xyz>
2022-06-24 14:55:48 -0700

    change sol file

---
+++ src/MyToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20{
+
+    constructor (string memory _name, string memory _symbol) ERC20 (_name,_symbol){
+    }
+
+    function mint(address to, uint256 amount) public virtual {
+        _mint(to,amount);
+    }
+
+    function burn(address from, uint amount) public virtual {
+        _burn(from, amount);
+    }
+
+
+}
